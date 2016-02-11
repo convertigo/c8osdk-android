7063418142d7e5700fd0b1334cd0d401a3f28a2a
Nicolas Albert <nicolasa@convertigo.com>
2016-02-11 14:31:36 +0000

    InProgress #4940 - Prepare JUnit testing for the client SDK Android
      * package moved
      * junit4 WTF hack
    
    
    Former-commit-id: b64374ad558e6660427b882874552d9bee1e78f3
    Former-commit-id: f4fcf59518581adbaec596603ac78dfe827d54cd
    Former-commit-id: 31c6dd936bfe00662adf353824315c49bb68c07d

---
+++ C8oSDKAndroid/c8oSDKJUnit/src/androidTest/java/com/convertigo/clientsdkjunit/ApplicationTest.java
@@ -0,0 +1,49 @@
+package com.convertigo.clientsdkjunit;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.convertigo.clientsdk.C8o;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+
+import javax.xml.xpath.XPathFactory;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public ApplicationTest() {
+        super(MainActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    }
+
+    @Test
+    public void testWTF() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void C8oDefault() throws Throwable {
+        final C8o c8o = new C8o(getActivity(), "http://pulse.twinsoft.fr:18080/convertigo/projects/Sample05");
+        Document doc = c8o.callXml(".sample05.GetServerInfo").sync();
+        assertNotNull(doc);
+        String uuid = XPathFactory.newInstance().newXPath().evaluate("//uuid/text()", doc);
+        assertNotNull(uuid);
+    }
+
+    @Test
+    public void toto() throws Exception {
+        assertEquals(6, 3 * 2);
+    }
+}
