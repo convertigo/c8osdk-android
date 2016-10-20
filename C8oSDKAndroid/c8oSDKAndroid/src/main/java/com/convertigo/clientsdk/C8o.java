63f5d8e4394e25d0c25c6c4ae82d925b73ed046a
Nicolas Albert <nicolasa@convertigo.com>
2016-10-20 09:05:10 +0000

    Start 2.0.8
    Former-commit-id: faaed2bd286deb136b257470fbe33231d4b585ea
    Former-commit-id: bf975360be9687fbf6b4f2fd6edf58abde08f660
    Former-commit-id: d76c90812dd25cf2bf3a7d579318cf938908c53e

--- C8oSDKAndroid/c8oSDKAndroid/src/main/java/com/convertigo/clientsdk/C8o.java
+++ C8oSDKAndroid/c8oSDKAndroid/src/main/java/com/convertigo/clientsdk/C8o.java
@@ -160,7 +160,7 @@ public class C8o extends C8oBase {
      * @return Current version of the SDK as "x.y.z".
      */
     public static String getSdkVersion() {
-        return "2.0.7";
+        return "2.0.8";
     }
 
     static final Executor executor;
